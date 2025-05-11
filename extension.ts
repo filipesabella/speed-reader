e8792b9d877abdddf54e1b98517368db0f72ab3b
Sheldon Corkery <sheldonc@iastate.edu>
2025-05-10 22:33:49 -0500

    Update yarn packages and work on updating to manifest v3

---
+++ extension.ts
@@ -0,0 +1,45 @@
+import browser from "webextension-polyfill";
+import { defaultSettings, Settings } from "./src/main/Settings";
+
+declare global {
+  interface Window {
+    speedReaderSettings: Settings;
+  }
+}
+
+browser.runtime.onInstalled.addListener(() => {
+  browser.contextMenus.create({
+    id: "speed-reader",
+    title: "Speed Reader",
+    contexts: ["selection"],
+    onclick: () => {
+      runSpeedReader();
+    },
+  });
+});
+
+async function runSpeedReader(): Promise<void> {
+  const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+  if (!tab?.id) return;
+
+  const settings = await browser.storage.sync.get("speed-reader-settings");
+  const finalSettings: Settings = {
+    ...defaultSettings,
+    ...(settings["speed-reader-settings"] || {}),
+  };
+
+  await browser.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: (settings: Settings) => {
+      window.speedReaderSettings = settings;
+    },
+    args: [finalSettings],
+  });
+
+  await browser.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["/build/speed-reader.js"],
+  });
+}
+
+browser.browserAction.onClicked.addListener(runSpeedReader);
